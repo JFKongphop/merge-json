70343dd4b396f0f9543a93a0158d40e3aaaacb5c
JFKongphop <kongphopleo@gmail.com>
2024-07-18 21:53:22 +0700

    create oneline

--- Makefile
+++ Makefile
@@ -3,3 +3,6 @@ cf:
 
 read:
 	node read.mjs
+
+ol:
+	node oneline.mjs
---
+++ oneline.mjs
@@ -0,0 +1,47 @@
+import fs from 'fs';
+import path from 'path';
+
+const convertFilesToSingleLine = (srcFolderPath, destFolderPath) => {
+  if (!fs.existsSync(destFolderPath)) {
+    fs.mkdirSync(destFolderPath, { recursive: true });
+  }
+
+  fs.readdir(srcFolderPath, (err, files) => {
+    if (err) {
+        return console.error('Unable to scan directory: ' + err);
+    }
+
+    files.forEach((file) => {
+      const srcFilePath = path.join(srcFolderPath, file);
+      const destFilePath = path.join(destFolderPath, file);
+      fs.stat(srcFilePath, (err, stats) => {
+        if (err) {
+          return console.error('Unable to get file stats: ' + err);
+        }
+        if (stats.isFile() && path.extname(file) === '.json') {
+          fs.readFile(srcFilePath, 'utf8', (err, data) => {
+            if (err) {
+              return console.error('Unable to read file: ' + err);
+            }
+            try {
+              const jsonData = JSON.parse(data);
+              const singleLineJson = JSON.stringify(jsonData);
+              fs.writeFile(destFilePath, singleLineJson, (err) => {
+                if (err) {
+                  return console.error('Unable to write file: ' + err);
+                }
+                console.log(`Converted ${file} to single line JSON and saved to ${destFilePath}`);
+              });
+            } catch (parseErr) {
+              console.error(`Unable to parse JSON in file ${file}: ` + parseErr);
+            }
+          });
+        }
+      });
+    });
+  });
+}
+
+const srcFolderPath = './example';
+const destFolderPath = './oneline';
+convertFilesToSingleLine(srcFolderPath, destFolderPath);
